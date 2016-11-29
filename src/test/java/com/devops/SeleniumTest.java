fe98eb7f8af5a924101bb47a6bbb0c04842bf2e7
CreditCardApplication <vieenodp@gmail.com>
2016-11-29 18:18:16 +0530

    Update SeleniumTest.java

--- src/test/java/com/devops/SeleniumTest.java
+++ src/test/java/com/devops/SeleniumTest.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SeleniumTest {
