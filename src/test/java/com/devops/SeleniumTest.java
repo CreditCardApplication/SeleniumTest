30883e4630b4c4fcf20b7f502553b8e159343c35
CreditCardApplication <vieenodp@gmail.com>
2016-11-29 18:50:31 +0530

    Update SeleniumTest.java

--- src/test/java/com/devops/SeleniumTest.java
+++ src/test/java/com/devops/SeleniumTest.java
@@ -29,7 +29,7 @@ public class SeleniumTest {
         DesiredCapabilities dCaps = new DesiredCapabilities();	  
         dCaps.setJavascriptEnabled(true);
 	dCaps.setCapability("takesScreenshot", false);
-	dCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/opt/phantomjs/bin");
+	dCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/opt/phantomjs/bin/phantomjs");
         WebDriver driver = new PhantomJSDriver(dCaps);
         //WebDriver driver = new FirefoxDriver();
         Thread.sleep(10000L);
